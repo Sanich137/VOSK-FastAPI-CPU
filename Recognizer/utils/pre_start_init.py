635f2ebd1a0bfdfff1ec75623d5dbe2c28f194d7
kojevnikov-office <Sanich137@gmail.com>
2024-02-09 19:43:07 +0300

    проба настройки пути БД для linux

--- Recognizer/utils/pre_start_init.py
+++ Recognizer/utils/pre_start_init.py
@@ -10,31 +10,24 @@ import uuid
 from datetime import datetime as dt
 from config import buffer_size
 
-
-
-
-
-
-
 auth_token = os.environ.get("AUTH_TOKEN")  # Не очень безопасно. Не для промышленного использования.
-unix_db = os.environ.get("DB_PATH")# linux
-BASE_DIR = Path(__file__).resolve().parent.parent
+unix_db = Path(os.environ.get("DB_PATH"))  # linux
+Win_base_dir = Path(__file__).resolve().parent.parent
 
 if platform == "linux" or platform == "linux2":
-    db = unix_db+'recogniser.db'
+    db = unix_db / 'recogniser.db'
 elif platform == "darwin":
     pass
 elif platform == "win32":
-    db = BASE_DIR / 'recogniser.db'
-
+    db = Win_base_dir / 'recogniser.db'
 
 paths = {
-    "BASE_DIR": BASE_DIR,
-    "test_file": BASE_DIR / 'content' / 'g_audio.mp3',
-    "trash_folder": BASE_DIR / 'trash',
-    "audio_archive_folder": BASE_DIR.parent / 'Audio_archive_folder',
-    "model_dir_small": BASE_DIR.parent / 'vosk_models_files' / 'vosk-model-small-ru-0.22',
-    "model_dir_complete": BASE_DIR.parent / 'vosk_models_files' / 'vosk-model-ru-0.42',
+    "BASE_DIR": Win_base_dir,
+    "test_file": Win_base_dir / 'content' / 'g_audio.mp3',
+    "trash_folder": Win_base_dir / 'trash',
+    "audio_archive_folder": Win_base_dir.parent / 'Audio_archive_folder',
+    "model_dir_small": Win_base_dir.parent / 'vosk_models_files' / 'vosk-model-small-ru-0.22',
+    "model_dir_complete": Win_base_dir.parent / 'vosk_models_files' / 'vosk-model-ru-0.42',
     "db": db,
 }
 
